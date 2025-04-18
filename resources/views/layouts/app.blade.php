b55979c06ac615c3712389d74ddb243cf7f201fb
Gerijacki <gerardloriz05@gmail.com>
2025-04-18 12:18:55 +0200

    refactor: Clean up HTML by removing commented Bootstrap CSS and JS sections

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>StegnonFiles</title>
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        /* Estilos personalizados para el modo oscuro */
         html body {
             background-color: #121212;
             color: #e0e0e0;
@@ -91,7 +89,6 @@
     </footer>
 
 
-    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
